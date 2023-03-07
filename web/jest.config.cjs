f3342d5531cd0be1704eaf51a6223a6b2851a2af
Retr0 <18603664+2Retr0@users.noreply.github.com>
2023-03-06 16:14:11 -0800

    Implement basic testing environment.
    Can be run with `npm run test`

---
+++ web/jest.config.cjs
@@ -0,0 +1,29 @@
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+    // Add more setup options before each test is run
+    setupFilesAfterEnv: [ '<rootDir>/src/utils/jest.setup.ts' ],
+    testEnvironment: 'jest-environment-jsdom',
+    transform: { 
+        "^.+\\.tsx$": "ts-jest", 
+        "^.+\\.ts$": "ts-jest" 
+    },
+    moduleNameMapper: { 
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/file_mock.js", 
+        "\\.(css|less)$": "<rootDir>/src/__mocks__/style_mock.js" 
+    },
+    collectCoverage: true,
+    clearMocks: true,
+    coverageDirectory: "coverage",
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
